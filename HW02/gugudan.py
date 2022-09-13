62bf0e0a0266e5d4d3842f2c8b0d5892ab236fc4
DaYoung Kang <93754504+RIVERALLZERO@users.noreply.github.com>
2022-09-13 12:28:47 +0900

    함수로 빼기

--- HW02/gugudan.py
+++ HW02/gugudan.py
@@ -1,7 +1,11 @@
-def main():
-    dan = 3
+def gugudan_print(dan):
     for i in range(9):
         print(f'{dan} * {i+1} = {dan * (i + 1)}')
+
+
+def main():
+    dan = int(input())
+    gugudan_print(dan)
 
 
 if __name__ == '__main__':
